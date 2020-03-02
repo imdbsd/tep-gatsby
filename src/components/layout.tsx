616a7176e350dc53b97a41ae87a1a30b7896e18e
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-03-02 20:24:46 +0700

    feat: rerun prettier

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,11 +5,11 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './layout.css'
 
 const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery(graphql`
