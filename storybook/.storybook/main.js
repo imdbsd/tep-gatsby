70e42e0b326ab0156f2899adba784b1e3bf93463
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-03-15 14:22:11 +0700

    add storybook config

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,3 +1,15 @@
+const path = require('path')
+
 module.exports = {
-  stories: ['../stories/*.stories.js'],
+  stories: ['../stories/*.stories.tsx'],
+  addons: [{
+    name: '@storybook/preset-typescript',
+    options: {
+      tsLoaderOptions: {
+        configFile: path.resolve(__dirname, '../../tsconfig.json'),
+      },
+      include: [path.resolve(__dirname, '../stories'), path.resolve(__dirname, '../../src')],
+      transpileManager: true,
+    },
+  },]
 }
